2946e69f88d1ee986c291afd7476ed33d7c9e2ce
kelta-king <kushangshah131@gmail.com>
2024-12-04 12:04:04 +0530

    Attempt to initialize electron-builder

--- src/app.js
+++ src/app.js
@@ -1,13 +1,8 @@
 const LOGGER = require("../Logger/logger");
 const { startServer } = require("./app-core/server");
 const { startWindows } = require("./app-window/windows");
-const CONFIG = require("./configuration/config");
 
 function application() {
-    if(CONFIG === undefined) {
-        LOGGER.fatal("Configuration is required for the application.");
-        return;
-    }
     LOGGER.info("Starting application...");
     startServer();
     startWindows();
